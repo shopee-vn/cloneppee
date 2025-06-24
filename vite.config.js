07b1a759bc47becf64344de67a606431ccbcd44d
shopee <mydev6868@gmail.com>
2025-06-24 08:48:24 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/peeeclone/',
+  base: '/peeeclon/',
   plugins: [react()],
 })
